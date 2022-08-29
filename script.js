999c65f3ad036d4a47c697ac26e4890f14ffdc9a
Karol Faltyn <karolfaltyn03@gmail.com>
2022-08-29 02:38:17 +0200

    added some js and a little to all

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function scroll_animation() {
     for (let i = 0; i < reveals.length; i++) {
         let windowHeight = window.innerHeight;
         let elementTop = reveals[i].getBoundingClientRect().top;
-        let elementVisible = 150;
+        let elementVisible = 300;
 
         if (elementTop < windowHeight - elementVisible) {
             reveals[i].classList.add("active");
@@ -15,3 +15,29 @@ function scroll_animation() {
     }
 }
 window.addEventListener("scroll", scroll_animation);
+
+// toggle dark mode
+const body = document.querySelector("body");
+const img = document.querySelector("img");
+
+const button = document.querySelector("#nav-icon")
+
+button.addEventListener('click', () =>{
+    
+    if(! button.classList.contains(".dark-mode-active")){
+        
+        body.style.cssText = "background-color: #181a1b; color: #FFF"
+        img.classList.add("img-shadow-dark-mode")
+        
+        
+        button.classList.add('.dark-mode-active')
+    }
+    else if(button.classList.contains(".dark-mode-active")){
+        
+        body.style.removeProperty("background-color")
+        body.style.removeProperty("color")
+        img.classList.remove("img-shadow-dark-mode")
+        
+        button.classList.remove('.dark-mode-active')
+    }
+})
